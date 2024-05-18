13cdf6d1679daf058dbc2ccedcf3c170080c730a
erleta05 <154436925+erleta05@users.noreply.github.com>
2024-05-18 14:46:34 +0200

    Update Program.cs

--- Detyra/Program.cs
+++ Detyra/Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("hello"); ;
-            Console.WriteLine("wrold");
+            Console.WriteLine("hello"); 
+            Console.WriteLine("world");
         }
     }
 }
